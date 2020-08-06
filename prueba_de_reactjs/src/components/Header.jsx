4c4588fd5ce452d4ab46fd41f8fe247191111d78
YamaguchiAlan <alanyamaguchi72@gmail.com>
2020-08-06 08:01:32 -0300

    Hecho el header

---
+++ prueba_de_reactjs/src/components/Header.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import './Header.css';
+
+let Header = () =>{
+    return(
+<div className="header">
+        <nav>
+            <div className="img-head">
+                <a href="#"><img src="https://cdn.pixabay.com/photo/2018/09/05/06/19/signal-3655575_960_720.png" alt="logo"></img></a>
+            </div>
+
+            <div className="search">
+                <form>
+                    <input type="text" placeholder="Buscar"/>
+                </form>
+                <i class="fas fa-search"></i>
+            </div>
+
+            <div className="head">
+            <a href="#" className="active">Inicio</a>
+            <a href="#">Noticias</a>
+            <a href="#">PC</a>
+            <a href="#">PS4</a>
+            <a href="#">XboxOne</a>
+            <a href="#">Switch</a>
+            <a href="#">IOS</a>
+            <a href="#">Android</a>
+            <a href="#">Foros</a>
+            <a href="#">Comunidad</a>
+            </div>
+            <hr></hr>
+        </nav>
+      </div>
+    )
+}
+
+export default Header;
---
+++ prueba_de_reactjs/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Nav.css';
+
+function Nav(){
+    return(
+<div className="primer">
+    <div className="Nav">
+        <a>fdah</a>
+    </div>
+</div>
+
+    )
+}
+
+export default Nav;
